22e1f2120a739102977fdf674c395d7002f36cba
Wandana Mddumage <wandanamuthumali99@gmail.com>
2024-09-21 19:50:41 +0530

    feat: add formEditor page menu

--- src/pages/FormEditor/components/Menu/components/Tabs/content/index.tsx
+++ src/pages/FormEditor/components/Menu/components/Tabs/content/index.tsx
@@ -1,41 +1,60 @@
 import { Button } from "@/components/ui/button";
+import { FaCloud } from "react-icons/fa";
 import { HiMiniStopCircle } from "react-icons/hi2";
 import { IoMdAdd } from "react-icons/io";
-import { MdOutlineMenu } from "react-icons/md";
+import { MdDelete, MdOutlineMenu } from "react-icons/md";
 
 const Content = () => {
   return (
-    <div>
+    <div className="mt-10">
       <div>
-        <h1 className="flex items-center gap-2 font-semibold text-sm">
-          <MdOutlineMenu /> Steps
-        </h1>
-        <div className="my-1 text-sm">
-          The steps users will take to complete the form
+        <div>
+          <h1 className="flex items-center gap-2 font-semibold text-sm">
+            <MdOutlineMenu /> Steps
+          </h1>
+          <div className="my-1 text-xs">
+            The steps users will take to complete the form
+          </div>
+        </div>
+        <div>
+          <Button
+            variant="ghost"
+            className="my-5 w-60 bg-zinc-50 hover:bg-zinc-100 flex font-normal text-xs"
+          >
+            <HiMiniStopCircle className="w-1/6 text-zinc-400" />
+            <div className="w-5/6 justify-center">Welcome screen</div>
+          </Button>
+          <Button
+            variant="outline"
+            className="my-5 hover:bg-zinc-100 flex font-medium text-xs gap-2 h-auto"
+          >
+            <IoMdAdd />
+            Add Field
+          </Button>
+          <hr className="my-8 border-t-2 border-zinc-100" />
+          <Button
+            variant="ghost"
+            className="w-60 bg-zinc-50 hover:bg-zinc-100 flex font-normal text-xs"
+          >
+            <HiMiniStopCircle className="w-1/6 text-zinc-400" />
+            <div className="w-5/6 justify-center">End screen</div>
+          </Button>
         </div>
       </div>
-      <div>
+      <div className="mt-64 flex gap-2 justify-center">
         <Button
           variant="ghost"
-          className="my-5 w-72 bg-zinc-50 hover:bg-zinc-100 flex font-normal text-sm"
+          className="w-full lg:w-1/2 bg-black hover:bg-zinc-900 flex font-medium text-xs text-white hover:text-white gap-2 h-auto"
         >
-          <HiMiniStopCircle className="absolute left-12 text-zinc-400" />
-          <div className="justify-center">Welcome screen</div>
+          <FaCloud className="text-white" />
+          <div className="justify-center">Save & Publish</div>
         </Button>
-        <Button
-          variant="outline"
-          className="my-5 hover:bg-zinc-100 flex font-normal text-sm gap-2 h-auto"
-        >
-          <IoMdAdd />
-          Add Field
-        </Button>
-        <hr className="my-8 border-t-2 border-zinc-100" />
         <Button
           variant="ghost"
-          className="w-72 bg-zinc-50 hover:bg-zinc-100 flex font-normal text-sm"
+          className="w-full lg:w-1/2 bg-white hover:bg-red-50 flex font-medium text-xs text-red-500 hover:text-red-500 gap-2 h-auto"
         >
-          <HiMiniStopCircle className="absolute left-12 text-zinc-400" />
-          <div className="justify-center">End screen</div>
+          <MdDelete className="text-red-500" />
+          <div className="justify-center">Delete</div>
         </Button>
       </div>
     </div>
