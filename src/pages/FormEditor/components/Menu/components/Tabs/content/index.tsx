2217565e82700ca28dda93cbf9e4fa8155c60b3d
Wandana Mddumage <wandanamuthumali99@gmail.com>
2024-09-21 21:05:16 +0530

    feat: add formEditor page menu

--- src/pages/FormEditor/components/Menu/components/Tabs/content/index.tsx
+++ src/pages/FormEditor/components/Menu/components/Tabs/content/index.tsx
@@ -1,10 +1,15 @@
 import { Button } from "@/components/ui/button";
+import { useState } from "react";
 import { FaCloud } from "react-icons/fa";
 import { HiMiniStopCircle } from "react-icons/hi2";
-import { IoMdAdd } from "react-icons/io";
+import { IoMdAdd, IoMdSettings } from "react-icons/io";
 import { MdDelete, MdOutlineMenu } from "react-icons/md";
+import CreateWelcomeScreen from "./components/WelcomScreen";
+import CreateEndScreen from "./components/EndScreen";
+import { RiCloseFill } from "react-icons/ri";
 
 const Content = () => {
+  const [isOpen, setIsOpen] = useState(false);
   return (
     <div className="mt-10">
       <div>
@@ -17,13 +22,53 @@ const Content = () => {
           </div>
         </div>
         <div>
-          <Button
-            variant="ghost"
-            className="my-5 w-60 bg-zinc-50 hover:bg-zinc-100 flex font-normal text-xs"
-          >
-            <HiMiniStopCircle className="w-1/6 text-zinc-400" />
-            <div className="w-5/6 justify-center">Welcome screen</div>
-          </Button>
+          <div>
+            <Button
+              variant="ghost"
+              className="my-5 w-60 bg-zinc-50 hover:bg-zinc-100 flex font-normal text-xs"
+              onClick={() => setIsOpen(true)}
+            >
+              <HiMiniStopCircle className="w-1/6 text-zinc-400" />
+              <div className="w-5/6 justify-center">Welcome screen</div>
+            </Button>
+            <div
+              className={`fixed inset-y-0 left-0 w-80 bg-white p-5 transform transition-transform duration-300 ease-in-out ${
+                isOpen ? "translate-x-0" : "-translate-x-full"
+              }`}
+            >
+              <div>
+                <Button
+                  variant="ghost"
+                  size="icon"
+                  className="absolute top-4 right-4 border"
+                  onClick={() => setIsOpen(false)}
+                >
+                  <RiCloseFill className="text-base font-bold" />
+                </Button>
+                <div className="flex items-center gap-2 text-sm font-medium mb-2">
+                  <IoMdSettings />
+                  Settings
+                </div>
+              </div>
+              <CreateWelcomeScreen />
+              <div className="mt-5 flex gap-2 justify-center">
+                <Button
+                  variant="ghost"
+                  className="w-full lg:w-1/2 bg-black hover:bg-zinc-900 flex font-medium text-xs text-white hover:text-white gap-2 h-auto"
+                >
+                  <div className="justify-center">Save</div>
+                </Button>
+                <Button
+                  variant="ghost"
+                  className="w-full lg:w-1/2 bg-white hover:bg-red-50 flex font-medium text-xs text-red-500 hover:text-red-500 gap-2 h-auto"
+                  onClick={() => setIsOpen(false)}
+                >
+                  <div className="justify-center">Discard</div>
+                </Button>
+              </div>
+            </div>
+          </div>
+
           <Button
             variant="outline"
             className="my-5 hover:bg-zinc-100 flex font-medium text-xs gap-2 h-auto"
@@ -31,14 +76,55 @@ const Content = () => {
             <IoMdAdd />
             Add Field
           </Button>
+
           <hr className="my-8 border-t-2 border-zinc-100" />
-          <Button
-            variant="ghost"
-            className="w-60 bg-zinc-50 hover:bg-zinc-100 flex font-normal text-xs"
-          >
-            <HiMiniStopCircle className="w-1/6 text-zinc-400" />
-            <div className="w-5/6 justify-center">End screen</div>
-          </Button>
+
+          <div>
+            <Button
+              variant="ghost"
+              className="w-60 bg-zinc-50 hover:bg-zinc-100 flex font-normal text-xs"
+              onClick={() => setIsOpen(true)}
+            >
+              <HiMiniStopCircle className="w-1/6 text-zinc-400" />
+              <div className="w-5/6 justify-center">End screen</div>
+            </Button>
+            <div
+              className={`fixed inset-y-0 left-0 w-80 bg-white p-5 transform transition-transform duration-300 ease-in-out ${
+                isOpen ? "translate-x-0" : "-translate-x-full"
+              }`}
+            >
+              <div className="">
+                <Button
+                  variant="ghost"
+                  size="icon"
+                  className="absolute top-4 right-4 border"
+                  onClick={() => setIsOpen(false)}
+                >
+                  <RiCloseFill className="text-base font-bold" />
+                </Button>
+                <div className="flex items-center gap-2 text-sm font-medium mb-2">
+                  <IoMdSettings />
+                  Settings
+                </div>
+              </div>
+              <CreateEndScreen />
+              <div className="mt-5 flex gap-2 justify-center">
+                <Button
+                  variant="ghost"
+                  className="w-full lg:w-1/2 bg-black hover:bg-zinc-900 flex font-medium text-xs text-white hover:text-white gap-2 h-auto"
+                >
+                  <div className="justify-center">Save</div>
+                </Button>
+                <Button
+                  variant="ghost"
+                  className="w-full lg:w-1/2 bg-white hover:bg-red-50 flex font-medium text-xs text-red-500 hover:text-red-500 gap-2 h-auto"
+                  onClick={() => setIsOpen(false)}
+                >
+                  <div className="justify-center">Discard</div>
+                </Button>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
       <div className="mt-64 flex gap-2 justify-center">
